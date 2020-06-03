ba33f6728796da87e6ef5cc7e40ac5671e49ba4e
David <46301704+DvdCp@users.noreply.github.com>
2020-06-04 00:52:43 +0200

    prova

---
+++ SurivalShooter/Assets/Scripts/LeantweenTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeantweenTest : MonoBehaviour
+{
+    public GameObject powerUpAlert;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Instantiate(powerUpAlert);
+        powerUpAlert.SetActive(false);
+        LeanTween.moveLocalY(gameObject,100,1.5f).setOnComplete(ActiveGameobject);
+    }
+
+    void DestroyThis()
+    {
+        Destroy(gameObject);
+    }
+
+    void ActiveGameobject()
+    {
+        powerUpAlert.SetActive(true);
+    }
+}
